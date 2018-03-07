a8570a180658bbd918db0e62de414faa1933038e
Nicolas Primeau <nicolas.primeau@gmail.com>
2018-03-06 23:45:04 -0500

    k

--- src/bismarck/command_processor/modules/base_commands.py
+++ src/bismarck/command_processor/modules/base_commands.py
@@ -4,13 +4,13 @@ from bismarck.command_processor.modules.base_io_controls import MouseControl
 def wakeup_screen():
     mouse = MouseControl()
     bounds = list(mouse.bounds)
-    center = [bounds[0]/2, bounds[1]/2]
+    center = [bounds[0]/2.0, bounds[1]/2.0]
     current_position = list(mouse.position)
     if center == current_position:
         jitter_mouse = (1, 1)
     else:
-        x = -1 if current_position[0] < bounds[0] else 1
-        y = 1 if current_position[1] < bounds[1] else -1
+        x = 1 if current_position[0] < center[0] else -1
+        y = 1 if current_position[1] < center[1] else -1
         jitter_mouse = (x, y)
     mouse.move_mouse_from_current(*jitter_mouse)
 
