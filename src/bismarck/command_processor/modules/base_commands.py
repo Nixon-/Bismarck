989247a6491694ad09e40fd47105c8eaa4a34f8b
Nicolas Primeau <nicolas.primeau@gmail.com>
2018-03-07 00:12:14 -0500

    Added basic mouse controls

--- src/bismarck/command_processor/modules/base_commands.py
+++ src/bismarck/command_processor/modules/base_commands.py
@@ -1,26 +1,24 @@
-from bismarck.command_processor.command_processor import CommandModule
 from bismarck.command_processor.modules.base_io_controls import MouseController
 
 
-class ScreenController(CommandModule):
+class Display:
 
-    SLEEP_COMMAND = 'gsettings set org.gnome.desktop.screensaver idle-activation-enabled false'
-    WAKE_UP_COMMAND = 'gsettings set org.gnome.desktop.screensaver idle-activation-enabled true'
+    def __init__(self):
+        self.mouse = MouseController()
 
     def wakeup(self):
-        mouse = MouseController()
-        bounds = list(mouse.bounds)
-        pos = mouse.position
+        bounds = list(self.mouse.bounds)
+        pos = self.mouse.position
         new_pos = [
             0 if pos[0] > bounds[0] / 2.0 < 0 else bounds[0],
             0 if pos[1] > bounds[1] / 2.0 < 0 else bounds[1]
         ]
-        mouse.move_mouse_to_absolute(*new_pos)
+        self.mouse.move_mouse_to_absolute(*new_pos)
 
 
 if __name__ == "__main__":
     import time
-    screen = ScreenController()
+    screen = Display()
     while True:
         screen.wakeup()
         time.sleep(30)
