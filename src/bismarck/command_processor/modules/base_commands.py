d1d39edba91692ef418a6966b52e5211b32efc32
Nicolas Primeau <nicolas.primeau@gmail.com>
2018-03-06 23:51:23 -0500

    k

--- src/bismarck/command_processor/modules/base_commands.py
+++ src/bismarck/command_processor/modules/base_commands.py
@@ -1,22 +1,20 @@
-from bismarck.command_processor.modules.base_io_controls import MouseControl
+import subprocess
 
 
 def wakeup_screen():
-    mouse = MouseControl()
-    bounds = list(mouse.bounds)
-    center = [bounds[0]/2.0, bounds[1]/2.0]
-    current_position = list(mouse.position)
-    if center == current_position:
-        jitter_mouse = (1, 1)
-    else:
-        x = 1 if current_position[0] < center[0] else -1
-        y = 1 if current_position[1] < center[1] else -1
-        jitter_mouse = (x, y)
-    mouse.move_mouse_from_current(*jitter_mouse)
+    proc = subprocess.Popen('gsettings set org.gnome.desktop.screensaver idle-activation-enabled false', shell=True)
+    proc.wait()
+
+
+def sleep_screen():
+    proc = subprocess.Popen('gsettings set org.gnome.desktop.screensaver idle-activation-enabled true', shell=True)
+    proc.wait()
 
 
 if __name__ == "__main__":
     import time
     while True:
         wakeup_screen()
-        time.sleep(65)
+        time.sleep(10)
+        sleep_screen()
+        time.sleep(10)
