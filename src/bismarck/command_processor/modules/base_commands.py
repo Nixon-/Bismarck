aa55792bac0cac45d8b43400256061105bcb2a82
Nicolas Primeau <nicolas.primeau@gmail.com>
2018-03-06 23:28:04 -0500

    k

--- src/bismarck/command_processor/modules/base_commands.py
+++ src/bismarck/command_processor/modules/base_commands.py
@@ -1,11 +1,13 @@
 from bismarck.command_processor.modules.base_io_controls import MouseControl
-
+import time
 
 def wakeup_screen():
     mouse_controller = MouseControl()
     jitter_mouse = (1, 1)
-    new_pos = mouse_controller.position
-    mouse_controller.move_mouse_to_absolute(new_pos[0]/2, new_pos[1]/2)
+    while True:
+        new_pos = mouse_controller.position
+        mouse_controller.move_mouse_to_absolute(new_pos[0]/2, new_pos[1]/2)
+        time.sleep(5)
     #mouse_controller.move_mouse_from_current(*jitter_mouse)
 
 
